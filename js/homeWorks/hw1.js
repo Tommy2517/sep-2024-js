771d32650181cb71eeb449a806e4a0f7e1455c5a
Tommy2517 <artem07stu@gmail.com>
2024-11-06 22:39:52 +0100

    resolve-homeWork-1

---
+++ js/homeWorks/hw1.js
@@ -0,0 +1,47 @@
+// #dYQNrBV
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+let var1 = 'hello';
+let var2 = 'owu';
+let var3 = 'com';
+let var4 = 'ua';
+let var5 = 1;
+let var6 = 10;
+let var7 = -999;
+let var8 = 123;
+let var9 = 3.14;
+let var10 = 2.7;
+let var11 = 16;
+let var12 = true;
+let var13 = false;
+console.log(var1,var2,var3,var4,var5,var6,var7,var8,var9,var10,var11,var12,var13);
+
+
+// #6Qb97gsv
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+let firstName = 'Artem';
+let middleName = 'Nikolaevich';
+let lastName = 'Lynevych';
+console.log(firstName + ' ' + lastName + ' ' + middleName);
+
+
+// #4N0y5tufA
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+let a = 100; let b = '100'; let c = true;
+console.log(typeof a, typeof b, typeof c);
+
+
+
+// #ruUtWDUI
+// Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// let firstNamePrompt = prompt('enter your firstName') 
+// let middleNamePrompt = prompt('enter your middleName')
+// let lastNamePrompt = prompt('enter your lastName')
+//  console.log(firstNamePrompt, middleNamePrompt, lastNamePrompt);
+
+
+
+

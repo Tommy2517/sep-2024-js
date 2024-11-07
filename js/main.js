e489f1e52e0859683a9cae46d127b013a534bca8
Tommy2517 <artem07stu@gmail.com>
2024-11-07 21:44:33 +0100

    edit main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // import {} from "./lessons/lesson1_vars_types_cats.js"
 // import {} from "./lessons/lesson2_arrays_objects_if.js"
+import {} from "./lessons/lesson3_loops.js"
 // import {} from "./homeWorks/hw1.js"
-import {} from "./homeWorks/hw2.js"
+// import {} from "./homeWorks/hw2.js"
